26ece0c14a45cb0d140723551b6ef6c7ba78877f
sharath2895 <assharath2895@gmail.com>
2020-09-06 19:03:56 +0530

    Angular assignment file 06.09.2020

--- assignment/angular-assignment/src/app/app.module.ts
+++ assignment/angular-assignment/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharathAssignmentComponent } from './sharath-assignment/sharath-assignment.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SharathAssignmentComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTooltipModule
+   
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- assignment/angular-assignment/src/app/sharath-assignment/sharath-assignment.component.ts
+++ assignment/angular-assignment/src/app/sharath-assignment/sharath-assignment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sharath-assignment',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SharathAssignmentComponent implements OnInit {
 
-  constructor() { }
+  private data: Observable<string>;
+  title: string = 'End User License Agreement'
+  subtitle: string = 'To Continue, you must agree to this agreement'
+  constructor() {
+
+  }
+  disagree(){
+    alert('you disagree to the license')
+    console.log("you disagree")
+    return
+  }
 
   ngOnInit(): void {
   }
